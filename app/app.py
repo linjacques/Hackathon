40799c186139d8202a230e00d9f0fae179f34a37
linjacques <linjacques463@gmail.com>
2025-09-24 10:13:14 +0200

    page à créer

--- app/app.py
+++ app/app.py
@@ -6,9 +6,14 @@ Page_accueil = st.Page(
     default=True
 )
 
+Page_Bruit = st.Page("page/exploration_bruit.py", "Analyse Bruit")
+Page_Vols = st.Page("page/exploration_vols.py", "Analyse Vols")
+Page_Carte = st.Page("page/carte_bruit.py", "Carte Folium")
+Page_Top = st.Page("page/top_origins.py", "Top Pays Origine")
+
 
 NavBarr = st.navigation(pages=[
-    Page_accueil
+    Page_accueil, Page_Bruit, Page_Vols, Page_Carte, Page_Top
 ])
 
 NavBarr.run()
